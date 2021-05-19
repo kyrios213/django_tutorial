71ab910107a02de5c83eb8b2a7c8b057cd50becb
Lance Pidor <kyrios213@gmail.com>
2021-05-19 10:48:25 +0800

    Updated the models

--- accounts/models.py
+++ accounts/models.py
@@ -10,6 +10,14 @@ class Customer(models.Model):
     def __str__(self):
         return self.name
 
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, null=True)
+
+    def __str__(self):
+        return self.name
+
+
 class Product(models.Model):
     CATEGORY = (
         ('Indoor', 'Indoor'),
@@ -19,8 +27,12 @@ class Product(models.Model):
     name = models.CharField(max_length=255, null=True)
     price = models.FloatField(null=True)
     category = models.CharField(max_length=255, null=True, choices=CATEGORY)
-    description = models.TextField(null=True)
+    description = models.TextField(null=True, blank=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
+    tags = models.ManyToManyField(Tag)
+
+    def __str__(self):
+        return self.name
 
 
 class Order(models.Model):
@@ -30,5 +42,7 @@ class Order(models.Model):
         ('Delivered', 'Delivered'),
     )
 
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, null=True)
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     status = models.CharField(max_length=255, null=True, choices=STATUS)
